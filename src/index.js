c8a13d049eb0d964347ae0c3fb0d4fced365b56a
bianbianzhu <bianbianzhu123@gmail.com>
2021-10-15 22:29:38 +1100

    123

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
